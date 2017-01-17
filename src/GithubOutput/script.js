c4dba3a1d497f06503e8be87c8271553c3970592
Lucie Mannechez <lucie@emedia-info.com>
2017-01-17 17:05:39 +0100

    init

---
+++ src/GithubOutput/script.js
@@ -0,0 +1,22 @@
+import bus from '../bus'
+
+export default {
+  name: 'GithubOutput',
+  created() {
+    bus.$on('new-username', this.onUsernameChange)
+  },
+  destroyed() {
+    bus.$off('new-username', this.onUsernameChange)
+  },
+  methods: {
+    onUsernameChange(name) {
+      this.currentUsername = name
+    }
+  },
+  data() {
+    return {
+      currentUsername: null,
+      githubData: {}
+    }
+  }
+}
